42d5038af207d0ffd629de1ca86e09ee96ac6c85
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-08 03:24:41 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,13 @@
 def find_max_value(array)
-  new_array = array.sort
-  array.sort[array.length-1]
+  #new_array = array.sort
+  #array.sort[array.length-1]
+  count = 0
+  max_value = -1
+  while count < array.length do
+    if array[count]> max_value
+      max_value = array[count]
+    end
+    count += 1
+  end
+  max_value
 end
